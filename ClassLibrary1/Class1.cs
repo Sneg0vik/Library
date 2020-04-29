ee0c6044fc08c74e6c765fee0d6de97e94e1ccef
Снеговой Алекс <sneg0vik732@gmail.com>
2020-04-29 12:46:30 +0300

    Добавьте файлы проекта.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {            
+
+        public static double AreaCalculator(double radius)
+        {            
+            return Math.PI * Math.Pow(radius, 2);
+        }      
+        
+        public static string TriangleCalculator(double a, double b , double c) 
+        {
+            double d = (a + b + c) / 2;
+            double result = Math.Sqrt(d * (d - a) * (d - b) * (d - c));
+            double bc = Math.Pow(b, 2) + Math.Pow(c, 2);
+            double ac = Math.Pow(a, 2) + Math.Pow(c, 2);
+            double ab = Math.Pow(a, 2) + Math.Pow(b, 2);
+            if ((Math.Pow(a, 2) == bc) || (Math.Pow(b, 2) == ac) || (Math.Pow(c, 2) == ab))
+            {
+                return $"Треугольник прямоугольный и его площадь состовляет:{Math.Round(result,3).ToString()}";
+            }
+            return $"Площадь треугольника:{Math.Round(result,3).ToString()}";
+            
+        }
+              
+    }    
+}
---
+++ ClassLibrary1/Monte.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Monte
+    {
+
+        public static double Calculation(int a, int b, int с)
+        {
+
+            double total = 0;
+            double yMax = 0;
+            double x;
+            double funct;
+
+            int i = 0;
+            do
+            {
+                x = GetRandomNumber(a, b);
+                funct = Math.Abs(Function(x));
+                if (yMax > funct)
+                {
+                    total += funct;
+                    i++;
+                }
+                else
+                {
+                    yMax = funct * 2;
+                    i = 0;
+                }
+            } while (i < с);
+
+            return Math.Sqrt(Math.Abs((b - a) * total / с));
+        }
+        public static double Function(double x)
+        {
+            return Math.Cos(x);
+        }
+        static double GetRandomNumber(double min, double max)
+        {
+            Random random = new Random();
+            return random.NextDouble()*(max - min) + min;
+        }
+    }
+}
+   
+    
